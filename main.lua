c9a0e37dd37a5c4443e97886d2041847f2d8c365
Bassim Hawa <s436437@ocsb.local>
2018-03-08 10:32:20 -0500

    yes

--- main.lua
+++ main.lua
@@ -1,7 +1,12 @@
 -----------------------------------------------------------------------------------------
---
--- main.lua
---
+-- Title: TouchAndReact
+-- Name: Bassim Hawa
+-- Course: ICS2O/3C
+-- This program displays a button. When you click the button it displays text as well as another object
 -----------------------------------------------------------------------------------------
 
--- Your code here
+--set background color
+display.setDefault ("background", 153/255, 204/255, 255/255)
+
+--hide status bar
+display.setStatusBar(display.HiddenStatusBar)
