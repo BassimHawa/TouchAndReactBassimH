a525fc53cdff8fa9bd49cdb5be2b59e3ab653785
Bassim Hawa <s436437@ocsb.local>
2018-03-08 11:05:06 -0500

    of course

--- main.lua
+++ main.lua
@@ -6,13 +6,62 @@
 -----------------------------------------------------------------------------------------
 
 --set background color
-display.setDefault ("background", 153/255, 204/255, 255/255)
+display.setDefault ("background", 0, 0, 0)
 
 --hide status bar
 display.setStatusBar(display.HiddenStatusBar)
 
 --create blue button, set its position and make it visible
-local blueButton = display.newImageRect("Images/blueButton.png", 198, 96)
+local blueButton = display.newImageRect("Images/bluebutton.png",198, 96)
 blueButton.x = display.contentWidth/2
 blueButton.y = display.contentHeight/2
-bluebutton
+blueButton.isVisible = true 
+
+--create red button, set its position and make it invisible
+local redButton = display.newImageRect("Images/redButton.png",198, 96)
+redButton.x = display.contentWidth/2
+redButton.y = display.contentHeight/2
+redButton.isVisible = false
+
+--create text objects, set its position and make it inivisble
+local textObject = display.newText ("Clicked!", 0, 0, nil, 50)
+textObject.x = display.contentWidth/2
+textObject.y = display.contentHeight/3
+textObject:setTextColor (1, 1, 1)
+textObject.isVisible = false
+
+--Function: BlueButtonListener
+--Input: touch listener
+--Output: none
+--Description: when blue button is clicked, it will make the text aooear with red button and make the blue button disappear
+local function BlueButtonListener(touch)
+	if (touch.phase == "began") then
+		blueButton.isVisible = false
+		redButton.isVisible = true
+		textObject.isVisible = true
+	end 
+
+	if (touch.phase == "ended") then
+		blueButton.isVisible = true
+		redButton.isVisible = false
+		textObject.isVisible = false
+	end
+end
+
+--Function: RedButtonListener
+--Input: touch listener
+--Output: none
+--Description: when red button is released, it will make the text disappear and red button
+--disappear, and then blue button reappear
+local function RedButtonListener(touch)
+
+	if (touch.phase == "ended") then
+		blueButton.isVisible = true
+		redButton.isVisible = false
+		textObject.isVisible = false
+	end
+end
+
+--add the respective listeners to each object
+blueButton:addEventListener("touch", BlueButtonListener)
+redButton:addEventListener("touch", RedButtonListener)
